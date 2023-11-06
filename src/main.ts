9835242876452dce0cbe7123bc630a72452fb15a
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2023-11-06 23:18:58 +0900

    [001] 삼각형 렌더링 코드 추가

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log("Hello");
+import Renderer from './renderer';
+
+const canvas = document.getElementById('gfx') as HTMLCanvasElement;
+canvas.width = canvas.height = 640;
+const renderer = new Renderer(canvas);
+renderer.start();
