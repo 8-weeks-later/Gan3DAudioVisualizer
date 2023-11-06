fda9a23ee0158adad26279975dad095075432e0f
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2023-11-06 22:58:28 +0900

    [001] node js + typescript 기반 프로젝트 설정

---
+++ webpack.ts
@@ -0,0 +1,72 @@
+import webpack from 'webpack';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import path from 'path';
+import { argv } from 'process';
+
+let env = process.env['NODE_ENV'];
+let isProduction =
+    (env && env.match(/production/)) ||
+    argv.reduce((prev, cur) => prev || cur === '--production', false);
+
+let config: webpack.Configuration = {
+    context: path.join(__dirname, 'src'),
+    entry: {
+        app: './main.ts'
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', 'js'],
+        modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true,
+                    compilerOptions: {
+                        isolatedModules: true
+                    }
+                }
+            },
+            {
+                test: /\.wgsl/,
+                type: 'asset/source'
+            }
+        ]
+    },
+    node: false,
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(
+                    isProduction ? 'production' : 'development'
+                )
+            }
+        })
+    ],
+    optimization: {
+        minimize: isProduction ? true : false
+    }
+};
+
+/**
+ * Start Build
+ */
+const compiler: webpack.Compiler = webpack(config);
+
+if (!argv.reduce((prev, cur) => prev || cur === '--watch', false)) {
+    compiler.run((err, stats) => {
+        if (err) return console.error(err);
+    });
+} else {
+    compiler.watch({}, (err, stats) => {
+        if (err) return console.error(err);
+    });
+}
