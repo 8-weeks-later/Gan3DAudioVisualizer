a42b37f0461f114136c06927b41b7a1eccd03497
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-11-25 14:37:15 +0900

    [004] 기본 렌더링 메시 박스로 변경

--- src/engine.ts
+++ src/engine.ts
@@ -20,7 +20,7 @@ export default class Engine {
 
         this.renderer.resizeBackings();
 
-        this.renderer.setMesh(geometryGenerator.makeTetrahedron(1));
+        this.renderer.setMesh(geometryGenerator.makeBox(1));
         this.renderer.render();
 
         while(1){
